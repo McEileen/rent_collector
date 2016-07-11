5ce389e3c75081bf12ac9e9a0556efeb429b9b37
Eileen McFarland and Michael Coonrod <pair+eileen+michael@allstate.com>
2016-07-11 14:48:05 -0500

    completed crud for renter

--- server/controllers/renters.js
+++ server/controllers/renters.js
@@ -1,10 +1,37 @@
 
-/* eslint-disable new-cap */
+/* eslint-disable new-cap, no-underscore-dangle */
 import express from 'express';
 import Renter from '../models/renter';
 const router = module.exports = express.Router();
+import bodyValidator from '../validators/renters/body';
+import paramsValidator from '../validators/renters/params';
 
 // index
 router.get('/', (req, res) => {
   Renter.find((err, renters) => res.send({ renters }));
+});
+
+// create
+router.post('/', bodyValidator, (req, res) => {
+  Renter.create(res.locals, (err, renter) => {
+    res.send({ renter });
+  });
+});
+
+// edit
+router.put('/:id', paramsValidator, bodyValidator, (req, res) => {
+  Renter.findByIdAndUpdate(req.params.id, res.locals, { new: true }, (err, renter) => {
+    res.send({ renter });
+  });
+});
+
+// delete
+router.delete('/:id', paramsValidator, (req, res) => {
+  Renter.findByIdAndRemove(req.params.id, (err, renter) => {
+    if (renter) {
+      res.send({ id: renter._id });
+    } else {
+      res.status(400).send({ messages: ['id not found'] });
+    }
+  });
 });
--- test/integration/renters.spec.js
+++ test/integration/renters.spec.js
@@ -1,11 +1,17 @@
-/* eslint-disable no-unused-expressions */
+/* eslint-disable no-unused-expressions, no-underscore-dangle */
 
+const mongoose = require('mongoose');
 const expect = require('chai').expect;
 const request = require('supertest');
 const app = require('../../dst/server');
-// const Renter = require('../../dst/models/renter');
+const Renter = require('../../dst/models/renter');
 
 describe('renters', () => {
+  beforeEach((done) => {
+    mongoose.connection.db.dropDatabase(() => {
+      done();
+    });
+  });
   describe('get /renters', () => {
     it('should return no renters', (done) => {
       request(app)
@@ -15,6 +21,68 @@ describe('renters', () => {
         expect(rsp.status).to.equal(200);
         expect(rsp.body.renters).to.have.length(0);
         done();
+      });
+    });
+  });
+  describe('post /renters', () => {
+    it('should create a new renter', (done) => {
+      request(app)
+      .post('/renters')
+      .send({ name: 'Mr. Rogers', money: 98500 })
+      .end((err, rsp) => {
+        expect(err).to.be.null;
+        expect(rsp.status).to.equal(200);
+        expect(rsp.body.renter.name).to.equal('Mr. Rogers');
+        expect(rsp.body.renter.money).to.equal(98500);
+        expect(rsp.body.renter.__v).to.not.be.null;
+        expect(rsp.body.renter._id).to.not.be.null;
+        done();
+      });
+    });
+    it('should not create a new renter - bad money', (done) => {
+      request(app)
+      .post('/renters')
+      .send({ name: 'Big Bird', money: 'f' })
+      .end((err, rsp) => {
+        expect(rsp.body.messages[0]).to.contain('"money" must be a number');
+        expect(rsp.status).to.equal(400);
+        done();
+      });
+    });
+  });
+  describe('put /renters/:id', () => {
+    it('should edit an existing renter', (done) => {
+      const r1 = new Renter();
+      r1.name = 'Oscar the Grouch';
+      r1.money = 25;
+      r1.save(() => {
+        request(app)
+        .put(`/renters/${r1._id}`)
+        .send({ name: r1.name, money: 0 })
+        .end((err, rsp) => {
+          expect(err).to.be.null;
+          expect(rsp.status).to.equal(200);
+          expect(rsp.body.renter.name).to.equal('Oscar the Grouch');
+          expect(rsp.body.renter.money).to.equal(0);
+          done();
+        });
+      });
+    });
+  });
+  describe('delete /renters/:id', () => {
+    it('should delete an existing renter', (done) => {
+      const r1 = new Renter();
+      r1.name = 'Elmo';
+      r1.money = 1;
+      r1.save(() => {
+        request(app)
+        .delete(`/renters/${r1._id}`)
+        .end((err, rsp) => {
+          expect(err).to.be.null;
+          expect(rsp.status).to.equal(200);
+          expect(rsp.body.id).to.equal(r1._id.toString());
+          done();
+        });
       });
     });
   });
